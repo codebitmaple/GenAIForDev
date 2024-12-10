2089bf30ee1e02c2483dabec7b5271897a436e52
Sanjeet Sahay <sanjeet@aarya.ai>
2024-12-10 11:00:25 -0800

    swift project

---
+++ sage/sage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sage
+//
+//  Created by Sanjeet on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sage/sage/SageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SageApp.swift
+//  Sage
+//
+//  Created by Sanjeet on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
